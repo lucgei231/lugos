35646be9ce25fdc3bf4bdc6e65c52496c4d613db
Lucas Geitgey <lucasgeitgey1611@gmail.com>
2025-03-01 13:14:50 +0000

    s

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 document.getElementById('test-button').addEventListener('click', () => {
     alert('Firebase is connected!');
 });
+function updateClock() {
+    const clock = document.getElementById('clock');
+    const now = new Date();
+    clock.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
